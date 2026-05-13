3d180dec4761cb5eb8d161def77cee5d04f9db14
arthurdantas629-sketch <arthurdantas629@gmail.com>
2026-05-13 14:31:30 +0000

    v1

--- teste.py
+++ teste.py
@@ -1,1 +1,24 @@
-
+#Criando um teste com pontuação--
+print("teste seus conhecimentos sobre ciências🔢 ")
+pontos = 0
+#--questão 1--
+print("qual é a formula da água?♒ ")
+print("a-H20\n b-c08\n c-AL")
+resposta1 = input("digite a resposta: ").lower()
+if resposta1 =="a":
+   print("resposta correta✅ ")
+   pontos = pontos +1
+else:
+   print("você errou!❌")
+#-questão 2--
+print("2-O sol é :☀️ ")
+print("a-satélite\n b-estrela\n c-asteróide")
+resposta2 = input("digite a resposta: ").lower()
+if resposta2 == "b":
+   print("voce acertou✅ ")
+   pontos = pontos + 1
+else:
+   print("você errou❌ ")
+print("fim do questionario")
+print(f"sua pontuação foi:{pontos}")
+#arthur dantas da silva--

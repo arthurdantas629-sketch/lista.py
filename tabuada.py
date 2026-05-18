1963d4479b358df675c29629da6ec18aa5251620
arthurdantas629-sketch <arthurdantas629@gmail.com>
2026-05-18 11:20:33 +0000

    v3

--- tabuada.py
+++ tabuada.py
@@ -1,1 +1,7 @@
+numero = int(input("tabuada do qual numero"))
+print(f"--- Tabuada do {numero} ---")
+for i in range(1, 11):
+    res = numero * i
+    print(f"{numero} x {i} = {res}")
 
+#arthur dantas da silva--

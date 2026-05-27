b188f729a74eeaafd0ae1023a74ab420d6905cfc
arthurdantas629-sketch <arthurdantas629@gmail.com>
2026-05-27 13:17:31 +0000

    v1

---
+++ cabecalho.py
@@ -0,0 +1,11 @@
+# definido e chamado a função
+def exibir_cabecalho():
+    print('=' * 30)
+    print('SISTEMA DE GESTÃO ESCOLAR')
+    print('Versão 1.0 - desenvolvido no codespaces')
+    print('=' * 30)
+
+    #chamado a função
+    exibir_cabecalho()
+    print('menu principal:')
+    

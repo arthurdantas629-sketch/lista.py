76aa2347c87f4652d2258f78f305313e07472576
arthurdantas629-sketch <arthurdantas629@gmail.com>
2026-06-22 11:14:54 +0000

    v8

--- painel.py
+++ painel.py
@@ -2,7 +2,8 @@ print("--- PAINEL DE ATENDIMENTO---")
 fila_espera = ("senha 01", "senha 02", "senha 03","senha 04")
 
 
-print(f"o proximo paciente a ser atendido é: {fila_espera[0]}")
-
-
+print(f"o proximo paciente a ser atendido é: {fila_espera[1]}")
+print(f"o proximo paciente a ser atendido é: {fila_espera[2]}")
+print(f"o proximo paciente a ser atendido é: {fila_espera[3]}")
+print(f"o proximo paciente a ser atendido é: {fila_espera[4]}")
 
---
+++ painel_interativo.py
@@ -0,0 +1,5 @@
+fila_espera=["senha 01", "senha 02", "senha 03", "senha 04"]
+senha_total = 0
+while senha_atual < len(fila_espera):
+    print("in=========")
+    print(f"senha atual:{fila_espera[atual - 1]}")
